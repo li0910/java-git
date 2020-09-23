5f484fdb2730aaa4232f5f9d41b40bc65e743359
li0910 <409422567@qq.com>
2020-09-23 13:10:22 +0800

    add Constructor

--- src/main/java/com/qf/pojo/user.java
+++ src/main/java/com/qf/pojo/user.java
@@ -3,4 +3,9 @@ package com.qf.pojo;
 public class user {
     private String name;
     private int age;
+
+    public user(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
 }
